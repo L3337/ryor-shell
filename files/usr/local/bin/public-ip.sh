81f9d01a3fb00960bb18c9ba29be97ccf69fcc6a
Anon McHacker <anonymous@noreply.L3337.github.com>
2023-08-27 19:47:30 -0700

    public ip: Don't hard code the wan interface

--- files/usr/local/bin/public-ip.sh
+++ files/usr/local/bin/public-ip.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+WAN_IFACE=$(python3 - << EOF
+import os
+import yaml
+with open(os.environ['CONFIG']) as f:
+    config = yaml.safe_load(f)
+print(config['wan_iface'])
+EOF
+)
+
 ip addr show dev enp6s0
